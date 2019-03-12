27e273aa2d5670c69f8a9235650516651d3f4d0f
Bob (FunFanker) <u211-03@public.mpei.local>
2019-03-12 16:36:20 +0300

    code: Нахождение минимума

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 using namespace std;
 
 int main()
-{
+{float minim;
     cout << "Enter A and B: ";
 int a, b;
 float maxim;
 cin >> a >> b;
+
 maxim=a;
+
+minim=a;
+
 cout << "A - B = " << a - b << '\n';
 cout << "A + B = " << a + b << '\n';
 cout << "A * B = " << a * b << '\n';
 cout << "A / B = " << a / b << '\n';
+
 if (maxim<b)
 maxim=b;
 cout <<"Maximym = "<< maxim;
+if (minim>b)
+    minim=b;
+cout << "Minimym = "<<minim;
 }
