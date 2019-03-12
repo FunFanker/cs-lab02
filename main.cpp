f1d1dd992e8a1a66ace296c8712cdfe6b71ac116
Alice (KozlyukDA) <alice@example.com>
2019-03-12 16:31:13 +0300

    code: нахождение максимума

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ int main()
 {
     cout << "Enter A and B: ";
 int a, b;
+float maxim;
 cin >> a >> b;
+maxim=a;
 cout << "A - B = " << a - b << '\n';
 cout << "A + B = " << a + b << '\n';
 cout << "A * B = " << a * b << '\n';
 cout << "A / B = " << a / b << '\n';
+if (maxim<b)
+maxim=b;
+cout <<"Maximym = "<< maxim;
 }
